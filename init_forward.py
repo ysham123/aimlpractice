cac64ad750b7c6415e03a8e9d1b80c9d28f8fb71
Yosef Shammout <YosefShammout123@gmail.com>
2025-12-25 19:43:18 -0500

    init and forward via pytorch, x and y included

--- init_forward.py
+++ init_forward.py
@@ -15,6 +15,9 @@ list(model.parameters())
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 loss_fn = nn.MSELoss()
 
+x = torch.randn(32, 2)
+y = torch.randn(32, 1)
+
 for epoch in range(100):
     optimizer.zero_grad()
     y_hat = model(x)
